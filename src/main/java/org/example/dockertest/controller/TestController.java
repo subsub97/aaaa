512a88c1fdf68fb58c23d44ff34d07b1a975cb70
Ho-Da-Dak <godqhr721@gmail.com>
2024-10-22 13:37:17 +0900

    upload my test File

---
+++ src/main/java/org/example/dockertest/controller/TestController.java
@@ -0,0 +1,15 @@
+package org.example.dockertest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok("Hello World");
+    }
+}
